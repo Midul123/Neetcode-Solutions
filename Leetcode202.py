c09080198cc22b4a35932359b483f5471bf12d4c
Midul123 <miduluddin03@gmail.com>
2024-08-04 18:57:51 +0100

    leetcode202

---
+++ Leetcode202.py
@@ -0,0 +1,14 @@
+#Leetcode202
+def isHappy(n):
+        trackN = []
+        while n !=1:
+            sumN = 0
+            for i in str(n):
+                sumN+=int(i)**2
+            if sumN in trackN:
+                return False
+                break
+            trackN.append(sumN)
+            n=sumN
+        return True
+print(isHappy(20))
