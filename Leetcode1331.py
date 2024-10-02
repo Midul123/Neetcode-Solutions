be0b3f42b7bd0c1e091451b757925c4560046608
Midul <miduluddin03@gmail.com>
2024-10-02 16:19:17 +0100

    leet1331

--- Leetcode1331.py
+++ Leetcode1331.py
@@ -4,4 +4,4 @@ def arrayRankTransform(arr):
     indexMap= {i:n for n,i in enumerate(sortedArr)}
     output = []
     return [indexMap[num]+1 for num in arr]
-print(arrayRankTransform([40,10,20,30]))
+print(arrayRankTransform([40,10,20,30,50]))
